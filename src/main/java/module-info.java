8669c08391e29fd6a9f007dbc607af613cff2182
mjfctor <emjayfactor@gmail.com>
2023-11-01 22:19:49 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.invetorysystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.invetorysystem to javafx.fxml;
+    exports com.example.invetorysystem;
+}
