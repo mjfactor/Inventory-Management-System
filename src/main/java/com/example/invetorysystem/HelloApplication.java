c94319a4a8fd19c4fd96945566d3aaa44e0d0734
mjfctor <emjayfactor@gmail.com>
2023-11-05 13:59:08 +0800

    Initial Commit

--- src/main/java/com/example/invetorysystem/HelloApplication.java
+++ src/main/java/com/example/invetorysystem/HelloApplication.java
@@ -52,6 +52,5 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-
     }
 }
--- src/main/java/com/example/invetorysystem/HelloController.java
+++ src/main/java/com/example/invetorysystem/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.invetorysystem;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button close;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button login_button;
+
+    @FXML
+    private AnchorPane main_frame;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField username;
+
+    public void close(){
+        System.exit(0);
     }
 }
