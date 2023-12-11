55cb82bb00d206b5ecc51e55be2b121e994350f6
mjfctor <emjayfactor@gmail.com>
2023-12-11 13:04:30 +0800

    Initial Commit

--- SQL/inventory.sql
+++ SQL/inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 05, 2023 at 10:59 AM
+-- Generation Time: Dec 11, 2023 at 06:03 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,6 +40,72 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 (1, 'admin', 'password');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `customerName` varchar(255) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `productName` varchar(100) NOT NULL,
+  `quantity` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `price_int` int(255) NOT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_receipt`
+--
+
+CREATE TABLE `customer_receipt` (
+  `transaction_id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `total` varchar(100) NOT NULL,
+  `paid` varchar(255) NOT NULL,
+  `balance` varchar(255) NOT NULL,
+  `total_int` int(11) NOT NULL,
+  `balance_int` int(11) NOT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer_receipt`
+--
+
+INSERT INTO `customer_receipt` (`transaction_id`, `customer_name`, `total`, `paid`, `balance`, `total_int`, `balance_int`, `date`) VALUES
+(2, 'MJ', '₱40,000', '₱40,000', '₱0', 40000, 0, '2023-12-10'),
+(3, 'sad', '₱30,000', '₱12,312', '₱0', 30000, 0, '2023-12-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `productName` varchar(100) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `price_int` int(255) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `productName`, `price`, `price_int`, `status`, `date`) VALUES
+(58, 'Mesa', '₱10,000', 10000, 'Available', '2023-12-03'),
+(59, 'SSS', '₱10,000', 10000, 'Available', '2023-12-03'),
+(74, 'QQQ', '₱10,000', 10000, 'Available', '2023-12-10');
+
 --
 -- Indexes for dumped tables
 --
@@ -51,6 +117,24 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer_receipt`
+--
+ALTER TABLE `customer_receipt`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,6 +143,24 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=224;
+
+--
+-- AUTO_INCREMENT for table `customer_receipt`
+--
+ALTER TABLE `customer_receipt`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
